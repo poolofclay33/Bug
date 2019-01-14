2ff8c7bb5be5d6993704c53b444f521cd295a170
Brady <poolofclay33@gmail.com>
2019-01-14 14:57:10 -0600

    ...

---
+++ A Bug Walks Up a Tree/Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float turnSpeed;
+
+    public GameObject tree;
+
+    public float speed;
+
+    void Update()
+    {
+        //transform.Rotate(Vector3.up, -turn * Time.deltaTime);
+
+        transform.RotateAround(tree.transform.position, tree.transform.up, -turnSpeed * Time.deltaTime);
+
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+}
