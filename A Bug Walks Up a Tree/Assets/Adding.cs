ab38397aef8be1055f9a27049c9f676b476fe90d
Brady <poolofclay33@gmail.com>
2019-01-28 13:38:18 -0600

    ...

---
+++ A Bug Walks Up a Tree/Assets/Adding.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adding : MonoBehaviour {
+
+    public GameObject _tree;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
